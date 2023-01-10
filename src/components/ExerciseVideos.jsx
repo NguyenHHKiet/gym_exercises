d6caa36d160087765597a7d53e120f15645eb7aa
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2023-01-10 21:50:07 +0700

    detail

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Typography, Box, Stack } from "@mui/material";
+import PropTypes from "prop-types";
 
-const ExerciseVideos = () => {
-  return <div>ExerciseVideos</div>;
+const ExerciseVideos = ({ exerciseVideo, name }) => {
+  return (
+    <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }} p="20px">
+      <Typography variant="h3" mb="33px">
+        Watch{" "}
+        <span
+          style={{
+            color: "#ff2625"
+          }}>
+          {name}
+          {` `}
+        </span>
+        exercise videos
+      </Typography>
+    </Box>
+  );
+};
+
+ExerciseVideos.propTypes = {
+  exerciseVideo: PropTypes.array.isRequired,
+  name: PropTypes.string.isRequired
 };
 
 export default ExerciseVideos;
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,25 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
-import { exercisesOptions, fetchData } from "../utils/fetchData";
+import { exercisesOptions, youtubeOptions, fetchData } from "../utils/fetchData";
 import Detail from "../components/Detail";
 import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+
   const { id } = useParams();
+
   useEffect(() => {
     const fetchExercisesData = async () => {
-      const exerciseDbUrl = "exercisedb.p.rapidapi.com";
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
       const youtubeSearchUrl = "https://youtube-search-and-download.p.rapidapi.com";
 
       const exerciseDetailData = await fetchData(
-        `${exerciseDbUrl}/exercises/${id}`,
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
         exercisesOptions
       );
       setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideoData = await fetchData(
+        `${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideoData);
     };
+
     fetchExercisesData();
   }, [id]);
 
@@ -28,7 +38,7 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
       <SimilarExercises />
     </Box>
   );
